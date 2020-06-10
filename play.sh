dfe816518c4ac878d9a82e04b31715ae95f85948
EpicNova-Hx <66078147+EpicNova-Hx@users.noreply.github.com>
2020-06-10 17:41:24 +0700

    Add files via upload

---
+++ play.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+#Halo Kang Recode
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #purple
+ku='\033[33;1m' #kuning
+clear
+figlet "WELCOME"
+echo $pur "Elite"$ku"V1"
+echo $cy"-----------------"$pur"-------------------"
+echo $cy" Author : Manca"
+echo $cy" Youtube : EpicNo"$pur"va"
+echo $cy" Whatsapp : 08880"$pur"2118339"
+echo $cy"-----------------"$pur"-------------------" 
+sleep 2
+echo $bi "M"$cy"asukan" $bi"P"$cy"assword"
+sleep 1
+read -p "[?] Password : " pil :
+#Batas Gan
+if [ $pil = CassaNova ]
+then
+clear
+fi
+echo $pur "Elite"$ku"V1"
+echo $cy"-----------------"$pur"-------------------"
+echo $cy" Author : Manca"
+echo $cy" Youtube : EpicNo"$pur"va"
+echo $cy" Whatsapp : 08880"$pur"2118339"
+echo $cy"-----------------"$pur"-------------------"
+read -p "[?] Choice : " fig :
+#Batas Gan
+if [ $fig = $fig ]
+then
+clear
+echo "Loading . . . "
+sleep 4
+figlet "$fig"
+echo "Sukses Dibuat ! "
+echo "Salin Aja Ya Ges"
+echo "Jangan Lupa Subscribe" $cy" EpicNova"
+fi
+#Batas Gan     
